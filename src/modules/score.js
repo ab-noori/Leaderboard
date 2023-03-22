8f8f911ad947dccabf9a1d5a0d52dcf8981254ed
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-03-23 00:28:05 +0430

    Add score module.

---
+++ src/modules/score.js
@@ -0,0 +1,11 @@
+// Task Class: Represents a Task
+
+class Score {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+    this.index = 0;
+  }
+}
+
+export default Score;
